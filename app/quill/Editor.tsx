f63d0efd13444d26c7fa8edc41c0b00315f7a8c2
Matthias Martin <github@stichwort-m.de>
2025-04-07 18:32:56 +0200

    quill support

--- app/quill/Editor.tsx
+++ app/quill/Editor.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
 import { useQuill } from "react-quilljs";
-// or const { useQuill } = require('react-quilljs');
 
 import "quill/dist/quill.snow.css"; // Add css for snow theme
 import Delta from "quill-delta";
-// or import 'quill/dist/quill.bubble.css'; // Add css for bubble theme
 
 export default function QuillEditor() {
   const { quill, quillRef, Quill } = useQuill({
